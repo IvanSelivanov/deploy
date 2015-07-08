4b0d7e9f128a0581958d831c547335bc5aa61bd8
Ivan Selivanov <selivanov@megratec.ru>
2015-07-08 19:47:10 +0300

    initial commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,8 @@ set :repo_url, 'git@github.com:IvanSelivanov/deploy.git'
 
 set :deploy_to, '/home/deploy/deploy'
 
-set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+#set :linked_files, %w{config/database.yml}
+#set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
 
